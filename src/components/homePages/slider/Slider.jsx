349292d65a00ef6895d81a1a6e855d4681453cd6
Murat İlhan <79536194+muratilhan@users.noreply.github.com>
2023-06-11 20:57:49 +0300

    swipe slider added

--- src/components/homePages/slider/Slider.jsx
+++ src/components/homePages/slider/Slider.jsx
@@ -1,7 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./slider.css";
-import { Carousel } from 'react-responsive-carousel';
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import res1 from '../../../pictures/sliders/mobile1.jpg'
 import res2 from '../../../pictures/sliders/mobile2.jpg'
 import res3 from '../../../pictures/sliders/mobile3.jpg'
@@ -9,50 +7,78 @@ import res4 from '../../../pictures/sliders/2.jpg'
 import res5 from '../../../pictures/sliders/4.jpg'
 import res6 from '../../../pictures/sliders/5.jpg'
 
+import "./slider.css"
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+
+
 
 function Slider() {
 
+    const [width, setWidth] = useState();
+
+    useEffect(()=>{
+      setWidth(window.innerWidth)
+    },[window])
+    useEffect(()=>{
+      console.log(width)
+    },[width])
+
+    const sliderPosts = [
+        {
+            id:"1",
+            img:width > 768 ? res4 : res1,
+            title:"asdas",
+            description:"asdasd"
+        },
+        {
+          id:"1",
+          img:width > 768 ? res5 : res1,
+          title:"asdas",
+          description:"asdasd"
+        },
+        {
+          id:"1",
+          img:width > 768 ? res6 : res1,
+          title:"asdas",
+          description:"asdasd"
+        },
+    ]
 
 
- 
+
+
   return (
-    <div className="slider-container slick-slider">
-        <Carousel className="main-slide slick-slider lg">
-            <div >
-                <img src={res4} alt="" height="630px" />
-                <p className="legend">Yenice Oto Kurtarma</p>
-            </div>
-            <div >
-                <img src={res5} alt="" height="600px" />
-                <p className="legend">Yenice Oto Kurtarma</p>
-            </div>
-            <div >
-                <img src={res6} alt="" height="600px" />
-                <p className="legend">Yenice Oto Kurtarma</p>
-            </div>
-
-           
-
-        </Carousel> 
-        <Carousel className="main-slide sm">
-            
-
-            <div >
-                <img src={res1} alt="" height="630px" />
-                <p className="legend">Yenice Oto Kurtarma</p>
-            </div>
-            <div >
-                <img src={res2} alt="" height="600px" />
-                <p className="legend">Yenice Oto Kurtarma</p>
-            </div>
-            <div >
-                <img src={res3} alt="" height="600px" />
-                <p className="legend">Yenice Oto Kurtarma</p>
-            </div>
-
-        </Carousel> 
+    <div className="slider-container">
+      <i className="fa-solid fa-arrow-left review-swiper-button-prev"></i>
+      <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        slidesPerView={1}
+        navigation={{
+          nextEl: '.review-swiper-button-next',
+          prevEl: '.review-swiper-button-prev',
+        }}  
+        loop={true}
+        className='swiper'
+      >
+        {
+          sliderPosts.map(p => (
+            <SwiperSlide key={p.id} className='swiper-slide'>
+              <div className="slide-img">
+                <img src={p.img} alt="" />
+              </div>
+            </SwiperSlide>
+          ))
+        }
+      </Swiper>
+      <i className="fa-solid fa-arrow-right review-swiper-button-next"></i>
     </div>
   );
 }
-
 export default Slider;
