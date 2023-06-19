f6272e62de0115202ec5c86b981b2814d7749be9
Murat İlhan <79536194+muratilhan@users.noreply.github.com>
2023-06-19 14:02:07 +0300

    robot file fixed

--- src/components/contact/maps/Maps.jsx
+++ src/components/contact/maps/Maps.jsx
@@ -5,7 +5,7 @@ import '../maps.css'
 export default function Maps() {
   return (
     <div className="map-container">
-      <iframe  frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas" src="https://maps.google.com/maps?width=555&amp;height=555&amp;hl=en&amp;q=inegol%20bursa%20osmaniye%20Bursa+(Oto%20Kurtarma)&amp;t=p&amp;z=13&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+      <iframe  frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas" src="https://maps.google.com/maps?width=555&amp;height=555&amp;hl=en&amp;q=inegol%20bursa%inegol%20Bursa+(Oto%20Kurtarma)&amp;t=p&amp;z=13&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
 
     </div>
   )
