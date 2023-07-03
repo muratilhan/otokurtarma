e475a00d39a35c99f44d2f1784b779039741fd5a
Murat İlhan <79536194+muratilhan@users.noreply.github.com>
2023-07-03 16:33:50 +0300

    html file updated

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -10,19 +10,19 @@ const Contact = () => {
             <h2>İletişim Bilgileri</h2>
               <li>
                 <span><i class="fa-solid fa-phone"></i></span>
-                <p><span>Telefon:</span>0533-344-86-50 <br /> 0536-722-47-07 </p>
+                <p><span className='contact-bold'>Telefon:</span> <p className='contact-info'>0533-344-86-50 <br /> 0536-722-47-07 </p> </p>
               </li>
               <li>
-                <span><i class="fa-brands fa-instagram"></i></span>
-                <p><span>İnstagram:</span>yenice_otokurtarma_emre</p>
+                <span><i class="fa-brands fa-square-instagram"></i></span>
+                <p><span className='contact-bold'>İnstagram:</span> <p className='contact-info'>yenice_otokurtarma_emre</p></p>
               </li>
               <li>
                 <span><i class="fa-solid fa-envelope"></i></span>
-                <p><span>Email:</span>Emreyucel016@outlook.com <br /> Onuryucel016@outlook.com </p>
+                <p><span className='contact-bold'>Email:</span> <p className='contact-info'> Emreyucel016@outlook.com <br /> Onuryucel016@outlook.com</p> </p>
               </li>
               <li>
                 <span><i class="fa-solid fa-location-dot"></i></span>
-                <p><span>Adres:</span>Osmaniye, Bursa Eskişehir Yolu No:2, 16400 İnegöl/Bursa</p>
+                <p><span className='contact-bold'>Adres:</span> <p className='contact-info'>Osmaniye, Bursa Eskişehir Yolu No:2, 16400 İnegöl/Bursa</p></p>
               </li>
           </ul>
           <Maps/>
